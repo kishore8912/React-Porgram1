5801dcedc9683299ecbb53c222fb95c89c2de30e
kishore8912 <kishorekumarreddy530@gmail.com>
2025-09-25 13:55:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from'react';
+import UserGreeting from'./UserGreeting';
+import UserInfo from'./UserInfo';
+import'./App.css';
+function App(){
+const user={
+name:'KISHORE',
+age:20
+};
+return(
+<div className="app-container">
+<div className="card">
+<h2>Sharing Data Using Props</h2>
+<UserGreeting name={user.name}/>
+<UserInfo name={user.name}age={user.age}/>
+</div>
+</div>
+);
+}
+export default App;
